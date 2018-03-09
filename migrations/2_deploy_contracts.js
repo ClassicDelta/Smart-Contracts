78684e22019fb6c531edde1b80d5327de1e08c68
ProphetDaniel <prophetdaniel@ethereumclassic.org>
2018-03-09 16:03:23 -0300

    :construction: Migration parameters are now set.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,17 @@
-// var Ownable = artifacts.require("Ownable");
+var ClassicDelta = artifacts.require("ClassicDelta");
 
 // NOTE: Use this file to easily deploy the contracts you're writing.
 //   (but make sure to reset this file before committing
 //    with `git checkout HEAD -- migrations/2_deploy_contracts.js`)
 
 module.exports = function (deployer) {
-  // deployer.deploy(Ownable);
+  const ownerAccount = "0x2cB00324a6B9Eb1756770beF05Dc83FE2D375090";
+  const admin = ownerAccount;
+  const feeAccount = ownerAccount;
+  const accountLevelsAddr = ownerAccount;
+  const feeMake = 0;
+  const feeTake = 0;
+  const feeRebate = 0;
+
+  deployer.deploy(ClassicDelta, admin, feeAccount, accountLevelsAddr, feeMake, feeTake, feeRebate);
 };
